ff4f69c87f2ff066e4f1c95156f5a8445a78b8ea
akhil-kanuri <akanuri@stu.naperville203.org>
2022-08-23 15:08:43 -0500

    learned blueJ and took notes

---
+++ MyFirstBlueJ/Ch1notes.java
@@ -0,0 +1,44 @@
+
+/**
+ * Akhil Kanuri
+ * 8/23/22
+ * Chapter 1 notes
+ * Class notes on basic concepts
+ * who helped me: Names
+ */
+public class Ch1notes
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Ch1notes
+     */
+    public Ch1notes()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * This is our code notes
+     */
+    public static void main(String[] args)
+    {
+        //basic data type
+        double number = 8.9,number2;
+        int x = 9,y,z;
+        boolean choice = false;
+        char letter = 'b';
+        float number3 = 8;
+        long n2 = 3;
+        short n3 = 7;
+        // these are classes and objects
+        //class  ID          Memory  Constructer
+        //name   
+        Integer   n1 =       new     Integer(47);
+        String    word =     new     String("Akhil Kanuri");
+        Double    number4 =  new     Double(6.7);
+        System.out.print(word.length());
+    }
+}
